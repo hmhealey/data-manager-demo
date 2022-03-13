8ce768f52271d5ad0129b5c2d6ddd8cfacd9e9a5
Harrison Healey <harrisonmhealey@gmail.com>
2022-03-13 11:22:13 -0400

    Add batchDebounce helper

---
+++ src/sagas/helpers.js
@@ -0,0 +1,37 @@
+import {actionChannel, put, race, take} from 'redux-saga/effects';
+
+export function wait(delay) {
+    console.log('waiting... ', delay);
+    return new Promise((resolve) => setTimeout(resolve, delay));
+}
+
+export function batchDebounce({incoming, outgoing, delay = 1000}) {
+    return function* () {
+        console.log('fetchThing', incoming, outgoing);
+        const incomingChannel = yield actionChannel(incoming);
+
+        while (true) {
+            const firstAction = yield take(incomingChannel);
+            console.log('starting first batch');
+
+            let batched = [firstAction];
+
+            while (true) {
+                const {action} = yield race({
+                    delay: wait(delay),
+                    action: take(incomingChannel),
+                });
+
+                if (action) {
+                    console.log('adding fetch');
+                    batched.push(action);
+                } else {
+                    break;
+                }
+            }
+
+            console.log('doing fetch for batch');
+            yield put({type: outgoing, batched});
+        }
+    };
+}
--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,50 +1,13 @@
-import {
-    actionChannel,
-    all,
-    put,
-    race,
-    take,
-    takeEvery,
-} from 'redux-saga/effects';
-
-function wait(delay) {
-    console.log('waiting... ', delay);
-    return new Promise((resolve) => setTimeout(resolve, delay));
-}
+import {all, put, takeEvery} from 'redux-saga/effects';
+import {batchDebounce, wait} from './helpers';
 
 // This debounces until it's been 1s since the last FETCH_THING before doing the fetching. I want to add a version
 // that throttles, but first, I should do something async to simulate an actual fetch and make sure that we can
 // handle a batch that's sent while a previous batch is processing. Blast processing.
-function* fetchThing() {
-    console.log('fetchThing');
-    const fetchChannel = yield actionChannel('FETCH_THING');
-
-    while (true) {
-        const firstAction = yield take(fetchChannel);
-
-        console.log('starting fetch batch');
-
-        let batched = [firstAction];
-
-        while (true) {
-            console.log('looping...');
-            const {action} = yield race({
-                delay: wait(1000),
-                action: take(fetchChannel),
-            });
-
-            if (action) {
-                console.log('adding fetch');
-                batched.push(action);
-            } else {
-                break;
-            }
-        }
-
-        console.log('doing fetch for batch');
-        yield put({type: 'DO_FETCH_THING', batched});
-    }
-}
+const fetchThing = batchDebounce({
+    incoming: 'FETCH_THING',
+    outgoing: 'DO_FETCH_THING',
+});
 
 function* doFetch(action) {
     console.log('fetching batch of ' + action.batched.length + '...');
